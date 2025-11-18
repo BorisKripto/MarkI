7e843e2e16dd830b3ffea3951903f4ac5318b954
AlanKharisov <alankharisov1@gmail.com>
2025-11-18 20:01:53 +0100

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,5 +1,5 @@
 // app.js — легкий API-клієнт з Bearer-токеном Firebase
-import { Auth } from "/js/firebase.js";
+import { Auth } from "./firebase.js";
 
 // якщо треба форс-дев для локальних тестів: постав true
 const DEV_ALLOW_XUSER_FALLBACK = false;
